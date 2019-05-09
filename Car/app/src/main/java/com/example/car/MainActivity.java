9280baa3e974d9c26ee31c677b6a48aa6a7e7db7
Sukhoskaya <Nastya-mz@tut.by>
2019-05-10 01:12:27 +0300

    final android app modifications done

--- Car/app/src/main/java/com/example/car/MainActivity.java
+++ Car/app/src/main/java/com/example/car/MainActivity.java
@@ -13,6 +13,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
@@ -164,12 +167,153 @@ public class MainActivity extends AppCompatActivity {
                 btnSpeedDown.setBackgroundColor(Color.BLUE);
             }
         });
+
+        btnUp.setOnTouchListener(new View.OnTouchListener() {
+           public boolean onTouch(View v, MotionEvent event) {
+               switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        up();
+                    break;
+                    case MotionEvent.ACTION_UP:
+                        mConnectedThread.write("C");
+                    break;
+                    }
+                return false;
+           }
+        });
+        btnStop.setOnLongClickListener(new View.OnLongClickListener() {
+            public boolean onLongClick(View v) {
+                mConnectedThread.write("C");
+                return false;
+                }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mConnectedThread.write("9");
+            }
+        });
+
+        btnLeft.setOnTouchListener(new View.OnTouchListener() {
+           public boolean onTouch(View v, MotionEvent event) {
+              switch (event.getAction()) {
+                  case MotionEvent.ACTION_DOWN:
+                      mConnectedThread.write("A");
+                      break;
+                  case MotionEvent.ACTION_UP:
+                      mConnectedThread.write("T");
+                  break;
+                  }
+              return false;
+           }
+        });
+
+        btnRight.setOnTouchListener(new View.OnTouchListener() {
+           public boolean onTouch(View v, MotionEvent event) {
+              switch (event.getAction()) {
+                 case MotionEvent.ACTION_DOWN:
+                    mConnectedThread.write("D");
+                  break;
+                 case MotionEvent.ACTION_UP:
+                    mConnectedThread.write("T");
+                 break;
+              }
+              return false;
+           }
+        });
+
+        public void onResume() {
+            super.onResume();
+            conectBluetooth();
+            }
+            
+        void conectBluetooth(){
+            Log.d(TAG, "...onResume - Попытка соединения ..");
+            // Set up a pointer to the remote node using it's address.
+            String address = "98:D3:33:80:56:5D";
+            BluetoothDevice device = btAdapter.getRemoteDevice(address);
+            // Two things are needed to make a connection:
+            //   A MAC address, which we got above.
+            //   A Service ID or UUID.  In this case we are using the
+            //     UUID for SPP.
+            try {
+                btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+                } catch (IOException e) {
+                     errorExit("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+               }
+
+            // Discovery is resource intensive.  Make sure it isn't going on
+            // when you attempt to connect and pass your message.
+            btAdapter.cancelDiscovery();
+            // Establish the connection.  This will block until it connects.
+            Log.d(TAG, "...Соединяемся..");
+                try {
+                   btSocket.connect();
+                   Log.d(TAG, "...Соединение установлено и готово к передачи данных...");
+                } catch (IOException e) {
+
+                    Log.d(TAG, "...Не удалось...");
+               }
+            // Create a data stream so we can talk to server.
+            Log.d(TAG, "...Создание Socket...");
+            mConnectedThread = new ConnectedThread(btSocket);
+            mConnectedThread.start();
+            }
+
+        public void onPause(){
+               super.onPause();
+               Log.d(TAG, "...In onPause()...");
+               try     {
+                   btSocket.close();
+                 } catch (IOException e2) {
+               errorExit("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
+                }
+            }
+                     
+            
+        private class ConnectedThread extends Thread { 
+            private final BluetoothSocket mmSocket;
+            //private final InputStream mmInStream;
+            private final OutputStream mmOutStream;
+            public ConnectedThread(BluetoothSocket socket) {
+                mmSocket = socket;
+                InputStream tmpIn = null;
+                OutputStream tmpOut = null;
+             // Get the input and output streams, using temp objects because
+             // member streams are final
+             try {
+                tmpOut = socket.getOutputStream();
+                 } catch (IOException e) {
+                 Log.d(TAG, "..Oшибка!!!!!!!!!!!!!!: " + tmpOut + "...");
+             }
+            
+            mmOutStream = tmpOut; 
+            }
+            /* Call this from the main activity to send data to the remote device */
+            public void write(String message) {
+               	Log.d(TAG, "..Данные для отправки: " + message + "...");
+                 	byte[] msgBuffer = message.getBytes();
+                   	try {
+                       mmOutStream.write(msgBuffer);
+                       Log.d(TAG, "..Данные для отправки: " + msgBuffer + "...");
+                        } catch (IOException e) {
+                       Log.d(TAG, "...Ошибка отправки данных: " + e.getMessage() + "...");
+                    conectBluetooth(); 
+                }
+            }
+            
+            public void cancel() {
+                 try {
+                      mmSocket.close();
+                     } catch (IOException e) { }
+            }
+        }
+    }}
+}
+
         private void errorExit(String fatal_error, String bluetooth_не_поддерживается){
             Toast.makeText(getBaseContext(), title + " - " + message, Toast.LENGTH_LONG).show();
             finish();
         }
     }
 }
-
-
-
